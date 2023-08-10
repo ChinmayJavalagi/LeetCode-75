07da7553e5b35d756bce0085da0c98769f784c80
Chinmay Avinash Javalagi <67673355+ChinmayJavalagi@users.noreply.github.com>
2023-08-10 10:35:54 +0530

    Find the Difference of Two Arrays

---
+++ 5. Hash Map/1. Find the Difference of Two Arrays.py
@@ -0,0 +1,12 @@
+'''
+Input: nums1 = [1,2,3], nums2 = [2,4,6]
+Output: [[1,3],[4,6]]
+Explanation:
+For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
+'''
+
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        setty1, setty2 = set(nums1), set(nums2)
+        return (list(setty1-setty2), list(setty2-setty1))
