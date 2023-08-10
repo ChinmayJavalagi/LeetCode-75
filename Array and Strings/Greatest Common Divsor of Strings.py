bd4582a1a2f7d253686e5e881905a696fb36d0b4
Chinmay Avinash Javalagi <67673355+ChinmayJavalagi@users.noreply.github.com>
2023-08-10 10:21:22 +0530

    Greatest Common Divsor of Strings

---
+++ Array and Strings/Greatest Common Divsor of Strings.py
@@ -0,0 +1,19 @@
+'''
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+'''
+
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        len1 = len(str1)
+        len2 = len(str2)
+        
+        def divisor(l):
+            if len1%l or len2%l:
+                return False
+            f1,f2 = len1//l , len2//l
+            return str2[0:l]*f1 == str1 and str2[0:l]*f2 == str2
+        for l in range(min(len1,len2),0,-1):
+            if divisor(l):
+                return str2[:l]
+        return ""
