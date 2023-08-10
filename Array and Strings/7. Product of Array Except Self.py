47926923cfe61089e54f0ef8b75c7c50a28de0e2
Chinmay Avinash Javalagi <67673355+ChinmayJavalagi@users.noreply.github.com>
2023-08-10 10:24:24 +0530

    Product of Array Except Self

---
+++ Array and Strings/7. Product of Array Except Self.py
@@ -0,0 +1,20 @@
+'''
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+'''
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        res = [1] * len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i] 
+        return res
